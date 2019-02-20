4032d52fa93c3b80eb666c2a60f48eae760a49aa
VIVEK RAJ <vivek@finoculus.com>
2019-02-20 16:50:54 +0530

    Auth-Process-Filter Changes added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
 import React, {Component} from 'react';
 import './App.css';
 import {Login} from "./login/Login";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {Home} from "./home/Home";
+import {Redirect} from "react-router";
+
 
 class App extends Component {
     render() {
         return (
+            <Router>
             <div className="App">
-
-
-                <Login />
-
-
+                <Route path="/login" component={Login} />
+                <PrivateRoute path="/home" component={Home} />
             </div>
+            </Router>
         );
     }
 }
 
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+        {...rest}
+        render={props =>
+            fakeAuth.isAuthenticated ? (
+                <Home />
+            ) : (
+                <Redirect
+                    to={{
+                        pathname: "/login",
+                        state: { from: props.location }
+                    }}
+                />
+            )
+        }
+    />
+);
+
+
+const fakeAuth = {
+    isAuthenticated: false,
+    authenticate(cb) {
+        this.isAuthenticated = true;
+        setTimeout(cb, 100); // fake async
+    },
+    signout(cb) {
+        this.isAuthenticated = false;
+        setTimeout(cb, 100);
+    }
+};
+
+
 export default App;
+
